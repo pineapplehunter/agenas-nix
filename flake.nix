1fc3fd1bbe519c66a2e2886e48c96170de70df6d
Shogo Takata <peshogo@gmail.com>
2025-05-29 22:04:22 +0900

    update deps

--- flake.nix
+++ flake.nix
@@ -77,4 +77,11 @@
 
       legacyPackages = eachSystem (p: p);
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://attic.s.ihavenojob.work/shogo" ];
+    extra-trusted-public-keys = [
+      "shogo:dzOG75ufKKljdUzTbGDpTuBmup3/K5RDmr28jb0jHCg="
+    ];
+  };
 }
